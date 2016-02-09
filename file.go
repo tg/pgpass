acc398fbd68973900a45d1ed0443666788d2d599
Tomasz Grodzki <dev@x7467.com>
2016-02-09 17:20:02 +0100

    Use $HOME for user's home directory if user.Current fails.
    This allows for cross-compiling code importing this library
    as user.Current uses cgo.

--- file.go
+++ file.go
@@ -6,12 +6,17 @@ import (
 	"path"
 )
 
-// OpenDefault opens default pgpass file, which is ~/.pgpass
+// OpenDefault opens default pgpass file, which is ~/.pgpass.
+// Current homedir will be retrieved by calling user.Current
+// or using $HOME on failure.
 func OpenDefault() (f *os.File, err error) {
+	var homedir = os.Getenv("HOME")
 	usr, err := user.Current()
-	if err != nil {
+	if err == nil {
+		homedir = usr.HomeDir
+	} else if homedir == "" {
 		return
 	}
 	// TODO: check file permission is 0600
-	return os.Open(path.Join(usr.HomeDir, ".pgpass"))
+	return os.Open(path.Join(homedir, ".pgpass"))
 }
